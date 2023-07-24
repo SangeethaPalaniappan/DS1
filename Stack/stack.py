9484848b13917774ec57f9c27a0ab4b00de84d73
SangeethaPalaniappan <122155534+SangeethaPalaniappan@users.noreply.github.com>
2023-07-24 22:05:45 +0530

    Updation 1 stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -33,12 +33,24 @@ class stack:
             
         else:
             print("Stack Empty")
-        
+    
+    def search(self,val):
+       temp=self.head
+       while temp!=None:
+          if temp.data==val:
+             print("Element found")
+             break        
+          else:
+             print("No element found")
+             break           
+               
 obj=stack()
 obj.push(10)
 obj.push(20)
 obj.push(30)
 obj.display()
+obj.search(30)
+obj.search(50)
 obj.pop()
 obj.pop()
 print("After popping:")
