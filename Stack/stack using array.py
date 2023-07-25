04ec35d8842b24bfd567d3661e8508475d3ca15f
SangeethaPalaniappan <122155534+SangeethaPalaniappan@users.noreply.github.com>
2023-07-25 12:50:26 +0530

    Top

--- Stack/stack using array.py
+++ Stack/stack using array.py
@@ -1,6 +1,5 @@
 #stack_using_array
 
-
 #Append
 
 def Append(arr,val):
@@ -17,54 +16,69 @@ def Append(arr,val):
             if i==len(arr)-1:
                arr+=[val] 
 
-def pop(arr):
+def Delete(arr,pos):
     s=len(arr)
     if s==0:
         print("List Empty")
-        return arr   
+        return arr
+    elif pos>=s:
+        print("index out of range")
+        return arr
     else:    
-        i=0
-        a=[]
-        while i!=s-1:
-            Append(a,arr[i])
-            i+=1
-        return a
+        for i in range(s):
+            
+            #To find the position
+            if pos==i:
+                for x in range(pos,s):
+                    
+                    #replacing the value 
+                    if x!=s-1:# s is the last index
+                        arr[x]=arr[x+1]
+                        
+                      
+                    else:
+                        #To delete the last element, reduced the size of the list and add the elements at the last position
+                        f=[]
+                        for t in range(s-1):
+                            Append(f,arr[t])  
+                        return f
 
-def search(n,val):
+def search(n,val,arr):
    for x in range(n):
       if arr[x]==val:
          print("Element Found")
          break
    else:
      print("No element found")
+
+class Stack:
+    def __init__(self):
+        self.top=-1
                     
-def stack_append(n,b):
-    for i in range(n):
-        s=int(input("s:"))
-        Append(b,s)
-    return b
+    def stack_push(self,n,b):
+        b=n*[-1]
+        
+        while n-1!=-1: 
+           s=int(input("s:"))       
+           self.top+=1
+           o=self.top       
+           b[o]=s      
+           n-=1       
+        return b
 
-
-
-
-
-
-
-
-
-
-
-def stack_pop(b):    
-    for i in range(1):
-        print(pop(b))
+    def stack_pop(self,b):    
+       while self.top!=-1:
+           b=Delete(b,len(b)-1)
+           self.top-=1
+           if self.top==-1:
+               print("Stack empty")         
+       return b              
+       
      
 n=int(input("n:"))
 arr=[]
-arr=stack_append(n,arr)
-search(n,50)
-search(n,40)
-arr=stack_pop(arr)
-if len(arr)==0:
-   print("List becomes empty")
-else:
-   print(arr)
+a=Stack()
+arr=a.stack_push(n,arr)
+search(n,50,arr)
+search(n,40,arr)
+arr=a.stack_pop(arr)
